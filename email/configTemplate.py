247f3375e24bcb2b04a4d94d1b856f9e9f94302f
Super-Service-ELF <super-service-elf@gmx.de>
2024-09-09 17:10:46 +0200

    Improve email/configTemplate.py

--- email/configTemplate.py
+++ email/configTemplate.py
@@ -2,5 +2,8 @@
     "link": "https://super-service-elf.de/newsletter-archiv/template",
     "message": "/contents/newsletter/template.html",
     "subject": "XX. ELF-Newsletter XXXX",
-    "recipients": ["mail1@example.com", "mail2@mailbox.org"],
+    "recipients": [
+        "mail1@example.com",
+        "mail2@example.com",
+    ],
 }
