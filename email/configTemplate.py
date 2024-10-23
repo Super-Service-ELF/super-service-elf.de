d5f64088b9ad2cd7c56e567626bbc395eef6ef97
Super-Service-ELF <mail@super-service-elf.de>
2024-10-23 20:17:22 +0200

    Fix code formatting

--- email/configTemplate.py
+++ email/configTemplate.py
@@ -1,8 +1,8 @@
 {
-    "link": "https://super-service-elf.de/newsletter-archiv/template",
-    "message": "/contents/newsletter/template.html",
-    "subject": "XX. ELF-Newsletter XXXX",
-    "recipients": [
-        "mail@example.com",
-    ],
+	"link": "https://super-service-elf.de/newsletter-archiv/template",
+	"message": "/contents/newsletter/template.html",
+	"subject": "XX. ELF-Newsletter XXXX",
+	"recipients": [
+		"mail@example.com",
+	],
 }
