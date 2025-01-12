06521b0972ebe752ea440d22acac1ec8166fab7f
Super-Service-ELF <mail@super-service-elf.de>
2025-01-12 14:10:18 +0100

    Remove trailing slashes from URLs

--- reminders/config.py
+++ reminders/config.py
@@ -4,7 +4,7 @@
 		"options": {
 			"body": "Erinnerungs-Text",
 			"data": {
-				"url": "https://super-service-elf.de/",
+				"url": "https://super-service-elf.de",
 			},
 			"icon": "https://super-service-elf.de/path/to/icon",
 			"requireInteraction": True,
