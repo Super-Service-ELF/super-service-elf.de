d18a5a7f9963357aab94982a37e53a3179498112
Super-Service-ELF <mail@super-service-elf.de>
2025-01-12 13:23:15 +0100

    Add reminder

--- reminders/config.py
+++ reminders/config.py
@@ -10,4 +10,13 @@
 			"requireInteraction": True,
 		},
 	},
+	"0": {
+		"title": "Heute schon mit der Technik geärgert?",
+		"options": {
+			"body": "Dann fragen Sie doch einfach den Super-Service-ELF!",
+			"data": {
+				"url": "https://super-service-elf.de/auftrag",
+			},
+		},
+	},
 }
